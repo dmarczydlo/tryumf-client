d77ec86393d05c7259ec0183a05825075b33ac5f
marczak <marczak1234@gmail.com>
2017-04-21 21:32:15 +0200

    test script

--- bin/build_php.sh
+++ bin/build_php.sh
@@ -6,6 +6,16 @@ cd $DIR_BIN
 cd ..
 cd public
 cat > index.php <<- "EOF"
-<?php include_once("index.html");?>
+<?php include_once("index.html");
+$files1 = scandir("./");
+echo "<pre>";
+print_r($files1);
+?>
+
 EOF
+
+cat > composer.json <<- "EOF"
+{}
+EOF
+
 echo 'SCRIPT DONE'
