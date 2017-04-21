cf24735fc92995c5ff4902602f0b09e27078bf46
marczak <marczak1234@gmail.com>
2017-04-21 22:26:49 +0200

    change script deploy

--- bin/remove.sh
+++ bin/remove.sh
@@ -4,11 +4,12 @@ DIR_BIN="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR_BIN
 cd ..
 
-rm node_modules
-rm src
+rm -rf node_modules
+rm -rf src
 rm .babelrc
 rm package.json
 rm README.md
 rm webpack-dev-server.config.js
 rm webpack-production.config.js
 mv public/* ./
+rm -rf public
