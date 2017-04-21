dfb8bbddd1184310dd692f94a669586508c39834
marczak <marczak1234@gmail.com>
2017-04-21 21:11:21 +0200

    test script

--- bin/build_php.sh
+++ bin/build_php.sh
@@ -5,11 +5,13 @@ DIR_BIN="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR_BIN
 cd ..
 cat > index.php <<- "EOF"
-<?php include_once("./public/index.html");
+<?php include_once("public/index.html");
  echo "start";
  $files1 = scandir("./");
+ echo "<pre>";
  print_r($files1);
  $files2 = scandir("./public/");
+ echo "<pre>";
  print_r($files2);
 ?>
 EOF
